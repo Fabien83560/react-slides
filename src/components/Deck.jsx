003488866697ce730b760ae2ba0aa09b1c16b136
Fabien83560 <fabienortega.290604@gmail.com>
2024-02-12 10:42:41 +0100

    Change text color when full-screen is activate

--- src/components/Deck.jsx
+++ src/components/Deck.jsx
@@ -1,20 +1,14 @@
 import Slide from "./Slide.jsx";
 import Button from "./Button.jsx";
+import ToolBar from "./ToolBar.jsx";
 
 export default function Deck({ isFullScreen, toggleFullScreen }) {
     const deckStyle = isFullScreen ? "m-0 shadow-none h-screen overscroll-none" : "m-10 shadow-2xl h-[82vh] overscroll-none";
     
-    const buttonTextColor = isFullScreen ? "text-black" : "text-white";
-
     return (
         <div className={deckStyle} style={{ position: 'relative' }}>
-            {isFullScreen && (
-                <div className="absolute bottom-4 left-0 m-4 z-50">
-                    <Button text="Exit Full Screen" onClick={toggleFullScreen} transparent={true} additionalClasses={buttonTextColor}
-                    />
-                </div>
-            )}
             <Slide />
+            <ToolBar toggleFullScreen={toggleFullScreen} isFullScreen={isFullScreen} />
         </div>
     );
 }
